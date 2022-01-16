75b8f702e11bf09229f50e4e77f3fc23511e79bc
LeeKangsoo <leeks733@naver.com>
2022-01-17 06:50:22 +0900

    First commit of Tutorial JavaScript
     - Console.log api, and alert
     - Differences of <script> is put into and "defer", "async" keyword

---
+++ Tutorial/1_class.js
@@ -0,0 +1,10 @@
+// whole-script strict mode syntax
+// Javascrpt is very flexible.
+// flexible == dangerous
+// added ECMAScript5
+
+'use strict';
+
+console.log("Hello world");
+let a = 1;
+console.log(a);
